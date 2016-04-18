3c5d72d2cdd3f68473a9d204c9d85fce2142c10e
rahuljain <rjain.iitr@gmail.com>
2016-04-18 12:01:47 +0530

    add upload script after compressing

--- logging/compresslog.sh
+++ logging/compresslog.sh
@@ -6,3 +6,5 @@ echo $dateValue
 #dateValue="2016-04-04"
 
 sudo tar cvzf $FileName /var/log/*/*$dateValue* /var/log/*$dateValue*   /etc/cinder/cinder.conf /etc/cinder/api-paste.ini /etc/ec2api/ec2api.conf /etc/ceph/ceph.conf /etc/haproxy/haproxy.cfg /etc/keepalived/keepalived.conf /etc/mysql/my.cnf /etc/oslo/matchmaker_ring.json /etc/telegraf/telegraf.conf /etc/supervisor/supervisord.conf /etc/hosts /etc/logrotate.d/*
+
+/home/ubuntu/loguploadtoDss.py
